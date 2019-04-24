5132dd49bea353adb0039615fa1e214b1010cc48
Jamis Moy <v-jamoy@microsoft.com>
2019-04-23 21:26:20 -0700

    completed homework2

--- Homework2Lists/Program.cs
+++ Homework2Lists/Program.cs
@@ -15,6 +15,29 @@ namespace Homework2Lists
             users.Add(new Models.User { Name = "Dave", Password = "hello" });
             users.Add(new Models.User { Name = "Steve", Password = "steve" });
             users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+
+            // Problem # 1
+            var helloUsers = users.Where(user => user.Password == "hello");
+            foreach (var x in helloUsers)
+            {
+                Console.WriteLine($"{x.Name} password is {x.Password}");
+            }
+
+            // Problem # 2
+            users.RemoveAll(user => user.Password == user.Name.ToLower());
+
+            // Problem # 3
+            // Remove() Only removes the first Occurance
+            users.Remove(helloUsers.First());
+
+            // Problem # 4
+            Console.Write("Remaining Users is: ");
+            foreach (var x in users)
+            {
+                Console.WriteLine($"{x.Name} password is {x.Password}");
+            }
+            Console.ReadLine();
         }
     }
 }
