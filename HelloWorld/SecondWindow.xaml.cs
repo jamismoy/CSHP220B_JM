06c66432b8d31e98371e7e4d1b84c24dba34a2c8
Jamis Moy <v-jamoy@microsoft.com>
2019-04-30 23:56:45 -0700

    Complete Homework 3 Sorting ListVIew

--- HelloWorld/SecondWindow.xaml.cs
+++ HelloWorld/SecondWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace HelloWorld
 {
@@ -19,17 +20,36 @@ namespace HelloWorld
     /// </summary>
     public partial class SecondWindow : Window
     {
+        CollectionView view;
         public SecondWindow()
         {
             InitializeComponent();
 
             var users = new List<Models.User>();
 
-            users.Add(new Models.User { Name = "Dave", Password = "DavePwd" });
-            users.Add(new Models.User { Name = "Steve", Password = "StevePwd" });
-            users.Add(new Models.User { Name = "Lisa", Password = "LisaPwd" });
-
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
             uxList.ItemsSource = users;
+
+            view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+
         }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            view.SortDescriptions.Clear();
+            string column = ((GridViewColumnHeader)e.OriginalSource).Column.Header.ToString();
+            switch (column)
+            {
+                case "Name":
+                    view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+                    break;
+                case "Password":
+                    view.SortDescriptions.Add(new SortDescription("Password", ListSortDirection.Ascending));
+                    break;
+            }
+        }
+
     }
 }
